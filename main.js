210edf2d4f28478a48fb38365b1b4f51f9705ae7
PedroNunesPagnussat <nppedronp@gmail.com>
2025-11-01 18:04:28 -0300

    folder collaps

--- main.js
+++ main.js
@@ -13,6 +13,24 @@ class FileExplorerTogglePlugin extends obsidian.Plugin {
             }
         });
 
+        // Add command for collapsing all folders
+        this.addCommand({
+            id: 'collapse-all-folders',
+            name: 'Collapse all folders',
+            callback: () => {
+                this.collapseAllFolders();
+            }
+        });
+
+        // Add command for expanding all folders
+        this.addCommand({
+            id: 'expand-all-folders',
+            name: 'Expand all folders',
+            callback: () => {
+                this.expandAllFolders();
+            }
+        });
+
         console.log('File Explorer Toggle plugin loaded');
     }
 
@@ -30,6 +48,40 @@ class FileExplorerTogglePlugin extends obsidian.Plugin {
             } else {
                 // Show the file explorer by expanding left sidebar
                 this.app.workspace.leftSplit.expand();
+            }
+        }
+    }
+
+    collapseAllFolders() {
+        const fileExplorer = this.app.workspace.getLeavesOfType('file-explorer')[0];
+        
+        if (fileExplorer && fileExplorer.view) {
+            const fileExplorerView = fileExplorer.view;
+            
+            // Collapse all folders by setting them to collapsed state
+            if (fileExplorerView.fileItems) {
+                Object.values(fileExplorerView.fileItems).forEach((item) => {
+                    if (item.file instanceof obsidian.TFolder) {
+                        item.setCollapsed(true);
+                    }
+                });
+            }
+        }
+    }
+
+    expandAllFolders() {
+        const fileExplorer = this.app.workspace.getLeavesOfType('file-explorer')[0];
+        
+        if (fileExplorer && fileExplorer.view) {
+            const fileExplorerView = fileExplorer.view;
+            
+            // Expand all folders by setting them to expanded state
+            if (fileExplorerView.fileItems) {
+                Object.values(fileExplorerView.fileItems).forEach((item) => {
+                    if (item.file instanceof obsidian.TFolder) {
+                        item.setCollapsed(false);
+                    }
+                });
             }
         }
     }
